4fc4c4f4b243e3332505a6a5afcd47135c7cc1a2
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-04-08 18:21:43 -0400

    Create Logout.py

---
+++ Logout.py
@@ -0,0 +1,14 @@
+import time
+import sys
+
+print("\n" * 41)
+print("Shutting down")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print("bye bye")
+sys.exit()
