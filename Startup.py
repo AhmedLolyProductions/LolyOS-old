79372e5347132010238f139f0050e8a87dda9bb0
Ahmed Loly Productions <268728928+AhmedLolyProductions@users.noreply.github.com>
2026-04-24 18:00:06 -0400

    Update Startup.py
    - Change from trinket.io to pickcode.io layout
    - Squash a bug

--- Startup.py
+++ Startup.py
@@ -3,124 +3,103 @@ from datetime import datetime
 import sys
 
 def Greeting():
-  #<settings>
-  Greeting_Status = 2
-  Custom = "Logging In..." #2
-  #</settings>
-  Logging = "Logging On..." #1
-  Loading = "Loading..." #0.5
-  Empty = "" #0
+    #<settings>
+    Greeting_Status = 2
+    Custom = "Logging In..." #2
+    #</settings>
+    Logging = "Logging On..." #1
+    Loading = "Loading..." #0.5
+    Empty = "" #0
+
+    now = datetime.now()
+    Current_Time = now.strftime("%a %b %d, %Y at %I:%M:%S %p") #docs.python.org/3/library datetime.html#format-codes
+    time.sleep(0.3)
   
-  now = datetime.now()
-  Current_Time = now.strftime("%a %b %d, %Y at %I:%M:%S %p") #docs.python.org/3/library/datetime.html#format-codes
-  time.sleep(0.3)
-  
-  if Greeting_Status in [0, 0.5, 1, 2]:
-      if Greeting_Status == 1:
-        time.sleep(0.4)
-        print(Logging)
-        time.sleep(0.6)
-        print(f"Today is {Current_Time}")
-        time.sleep(1.4)
-        print("\n")
-        import Apps
+    if Greeting_Status in [0, 0.5, 1, 2]:
+        if Greeting_Status == 1:
+            time.sleep(0.4)
+            print(Logging)
+            time.sleep(0.6)
+            print(f"Today is {Current_Time}")
+            time.sleep(1.4)
+            print("\n")
+            import Apps
       
-      elif Greeting_Status == 0.5:
-        time.sleep(0.4)
-        print(Loading)
-        time.sleep(0.6)
-        print(f"Today is {Current_Time}")
-        time.sleep(1.4)
-        print("\n")
-        import Apps
+        elif Greeting_Status == 0.5:
+            time.sleep(0.4)
+            print(Loading)
+            time.sleep(0.6)
+            print(f"Today is {Current_Time}")
+            time.sleep(1.4)
+            print("\n")
+            import Apps
+
+        elif Greeting_Status == 0:
+            time.sleep(0.4)
+            print(Empty)
+            time.sleep(0.6)
+            print(f"Today is {Current_Time}")
+            time.sleep(1.4)
+            print("\n")
+            import Apps
       
-      elif Greeting_Status == 0:
+        elif Greeting_Status == 2:
+            time.sleep(0.4)
+            print(Custom)
+            time.sleep(0.6)
+            print(f"Today is {Current_Time}")
+            time.sleep(1.4)
+            print("\n")
+            import Apps
+
+    else:
         time.sleep(0.4)
-        print(Empty)
+        print("")
         time.sleep(0.6)
         print(f"Today is {Current_Time}")
-        time.sleep(1.4)
-        print("\n")
-        import Apps
-      
-      elif Greeting_Status == 2:
-        time.sleep(0.4)
-        print(Custom)
-        time.sleep(0.6)
-        print(f"Today is {Current_Time}")
-        time.sleep(1.4)
-        print("\n")
-        import Apps
-  
-  else:
-    time.sleep(0.4)
-    print("")
-    time.sleep(0.6)
-    print(f"Today is {Current_Time}")
 def Password():
-  #<settings>
-  Password = "password"
-  #</settings>
-  counter = 0
-  time.sleep(0.6)
-  print("Loading...")
-  time.sleep(0.6)
-  print("Welcome!")
-  time.sleep(0.4)
-  print("Enter password")
-  Password_Input = input("Password:")
-  
-  if Password_Input == Password:
+    #<settings>
+    Password = "password"
+    #</settings>
+    counter = 0
+    time.sleep(0.6)
+    print("Loading...")
+    time.sleep(0.6)
+    print("Welcome!")
     time.sleep(0.4)
-    Greeting()
-  
-  else:
-    while counter < 4:
-      time.sleep(0.37)
-      print("\n")
-      print("Invalid password")
-      Password_Input = input("Password:")
-      counter += 1
-      
-      if Password_Input == Password:
+    print("Enter password")
+    Password_Input = input("Password:")
+
+    if Password_Input == Password:
         time.sleep(0.4)
         Greeting()
 
     else:
-      time.sleep(0.2)
-      print("Too many inncorrect attempts, importing the beast...")
-      time.sleep(0.5)
-      print("Installing virus.exe")
-      time.sleep(1)
-      import Virus
-def Startup():
-  counter = 0
-  time.sleep(1)
-  print("Startup the operating system? [Y/N]")
-  y_n = input("admin:/")
-  
-  if y_n in ['y', 'Y', 'yes', 'Yes']:
-    print("Starting LolyOS")
-    time.sleep(1)
-    print(".")
-    time.sleep(1)
-    print(".")
-    time.sleep(1)
-    print(".")
-    time.sleep(1)
-    Password()
-  
-  elif y_n in ['n', 'N', 'no', 'No']:
-    import Logout
-  
-  else:
-    while counter < 3:
-      print("\n")
-      print("Please try again")
-      y_n = input("admin:/")
-      counter += 1
+        while counter < 4:
+            time.sleep(0.37)
+            print("\n")
+            print("Invalid password")
+            Password_Input = input("Password:")
+            counter += 1
       
-      if y_n in ['y', 'Y', 'yes', 'Yes']:
+            if Password_Input == Password:
+                time.sleep(0.4)
+                Greeting()
+        time.sleep(0.2)
+        print("Too many inncorrect attempts, unauthorized anomoly detected")
+        time.sleep(4)
+        print("Initiating Loly Defender on full gaurd...")
+        time.sleep(5)
+        print("Warning: Loly Defender has been disabled")
+        time.sleep(5)
+        import Virus
+def Startup():
+    counter = 0
+    time.sleep(1)
+    print("Startup the operating system? [Y/N]")
+    y_n = input("admin:/")
+
+    if y_n in ['y', 'Y', 'yes', 'Yes']:
         print("Starting LolyOS")
         time.sleep(1)
         print(".")
@@ -130,12 +109,34 @@ def Startup():
         print(".")
         time.sleep(1)
         Password()
-      
-      elif y_n in ['n', 'N', 'no', 'No']:
+
+    elif y_n in ['n', 'N', 'no', 'No']:
         import Logout
-      
-      if counter == 3 and y_n not in ['y', 'Y', 'yes', 'Yes', 'n', 'N', 'No', 'no']:
-        print("Too many invalid inputs, shutting down...")
-        time.sleep(1)
-        import Logout
+
+    else:
+        while counter < 3:
+            print("\n")
+            print("Please try again")
+            y_n = input("admin:/")
+            counter += 1
+
+        if y_n in ['y', 'Y', 'yes', 'Yes']:
+            print("Starting LolyOS")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            print(".")
+            time.sleep(1)
+            Password()
+
+        elif y_n in ['n', 'N', 'no', 'No']:
+            import Logout
+
+        elif counter == 3 and y_n not in ['y', 'Y', 'yes', 'Yes', 'n', 'N', 'No', 'no']:
+            print("Too many invalid inputs, unauthorized anomoly detected")
+            time.sleep(4)
+            print("Warning: Virus.exe has gained")
+            import Logout
 Startup()
